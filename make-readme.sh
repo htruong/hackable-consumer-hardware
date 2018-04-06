95d03f4fadaa3408276ac068062804d06717a0a4
Huan Truong <htruong@tnhh.net>
2018-04-06 11:27:52 -0400

    [script] move some md file to csv

--- make-readme.sh
+++ make-readme.sh
@@ -3,8 +3,8 @@
 AUTOGEN_WARNING="**Warning**: This table is auto-generated from a bash script. Please do not edit\/submit pull requests to this file directly."
 
 sed -e "s/{{autogen_warning}}/${AUTOGEN_WARNING}/" README-source.md > README.md
-awk -F";" '$6 { print "|" $1 "|" $2 "|" $3 "|" $4 "|" $5 "|[" $6 "](#" $6 ")|" } !$6 { print "|" $1 "|" }' toh.md > toh_tmp.md
-awk -F";" '$6 { print "|" $1 "|" $2 "|" $3 "|" $4 "|" $5 "|[" $6 "](#" $6 ")|" } !$6 { print "|" $1 "|" }' hacks.md > hacks_tmp.md
+awk -F";" '$6 { print "|" $1 "|" $2 "|" $3 "|" $4 "|" $5 "|[" $6 "](#" $6 ")|" } !$6 { print "|" $1 "|" }' toh.csv > toh_tmp.md
+awk -F";" '$6 { print "|" $1 "|" $2 "|" $3 "|" $4 "|" $5 "|[" $6 "](#" $6 ")|" } !$6 { print "|" $1 "|" }' hacks.csv > hacks_tmp.md
 sed -ie '/{{toh\.md}}/rtoh_tmp.md' README.md
 sed -ie '/{{hacks\.md}}/rhacks_tmp.md' README.md
 sed -ie '/{{references\.md}}/rreferences.md' README.md
